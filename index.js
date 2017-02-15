5d8281e402bf8cda80e014755959c66fa4765d50
flyyang <pengfei_yang@outlook.com>
2017-02-16 03:02:50 +0800

    add conf parser

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const program = require('commander')
 const chokidar = require('chokidar')
 const exec = require('child_process').exec
 const colors = require('colors')
+const conf = require('./src/check-conf.js')
 
 program
   .version('0.0.1')
@@ -19,29 +20,30 @@ program
 
 const path2 = program.path
 
+conf.parseConf()
+process.exit()
 if (!path2) {
   program.help()
 }
 
 if (program.sync) {
-
   const watcher = chokidar.watch(path2, {
     ignored: /(^|[/\\])\..|node_modules\/|vendor\//,
     persistent: true,
     ignorePermissionErrors: true,
-    cwd: '.'
+    cwd: '.',
   })
 
   watcher
-    .on('change', path => {
-      path3 = path2.substring(path2.lastIndexOf('/'))
+    .on('change', (path) => {
+      const path3 = path2.substring(path2.lastIndexOf('/'))
       const relativePath = path.substring(path3.length + path.lastIndexOf(path3))
       const cmd = `sshpass -p yunshan3302 scp -P 61209 ${path} root@192.168.42.153:/var/www/lcweb${relativePath}`
 
       console.log(colors.green(cmd))
-      exec(cmd, (error, stdout, stderr) => {
+      exec(cmd, (error) => {
         if (error !== null) {
-            console.log(`exec error: ${error}`);
+          console.log(`exec error: ${error}`)
         }
       })
       console.log(colors.rainbow('copy files success'))
--- src/check-conf.js
+++ src/check-conf.js
@@ -1,2 +1,29 @@
-const yamlParser = require('js-yaml')
+const parser = require('js-yaml')
+const fs = require('fs')
+const os = require('os')
 
+const confDefaultPath = `${os.homedir()}/.easy-sync.rc`
+
+function confExists() {
+  return fs.existsSync(confDefaultPath)
+}
+
+function parseConf() {
+  console.log(confDefaultPath)
+  if (!confExists()) {
+    console.log('')
+    console.log('x'.bold.red, '.easy-syn.rc not found in user home path'.red)
+    console.log('')
+    process.exit(1)
+  }
+  try {
+    const doc = parser.safeLoad(fs.readFileSync(confDefaultPath, 'utf8'))
+    console.log(doc)
+  } catch (e) {
+    console.log(e)
+  }
+}
+module.exports = {
+  confExists,
+  parseConf,
+}
