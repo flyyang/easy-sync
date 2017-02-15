9e4983c08d68a4dbd8960f2c850490cf467ecc9a
pengfei <pengfei@yunshan.net.cn>
2017-02-15 18:55:21 +0800

    add sync tools

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const program = require('commander')
 const chokidar = require('chokidar')
+const exec = require('child_process').exec
+const colors = require('colors')
 
 program
   .version('0.0.1')
@@ -22,14 +24,27 @@ if (!path2) {
 }
 
 if (program.sync) {
-  // Initialize watcher.
-  const watcher = chokidar.watch(path2, {
+
+	const watcher = chokidar.watch(path2, {
     ignored: /(^|[/\\])\..|node_modules\/|vendor\//,
     persistent: true,
     ignorePermissionErrors: true,
+		cwd: '.'
   })
 
   watcher
-    .on('change', path => console.log(`File ${path} has been changed`))
+    .on('change', path => {
+			path3 = path2.substring(path2.lastIndexOf('/'))
+			const relativePath = path.substring(path3.length + path.lastIndexOf(path3))
+      const cmd = `sshpass -p yunshan3302 scp -P 61209 ${path} root@192.168.42.153:/var/www/lcweb${relativePath}`
+
+			console.log(colors.green(cmd))
+      exec(cmd, (error, stdout, stderr) => {
+				if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }
+      })
+      console.log(colors.rainbow('copy files success'))
+    })
     .on('error', error => console.log(`${error}`))
 }
